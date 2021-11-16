a67ed46b606dc01df494987400e63156d087e060
cgordon <cgordon@smtp.progeny.net>
2021-11-16 18:37:56 -0500

    added support for EstimateAffine2DWithParams

--- calib3d.cpp
+++ calib3d.cpp
@@ -63,6 +63,6 @@ Mat EstimateAffine2D(Point2fVector from, Point2fVector to) {
     return new cv::Mat(cv::estimateAffine2D(*from, *to));
 }
 
-//Mat EstimateAffine2DWithParams(Point2fVector from, Point2fVector to, PointVector inliers, int method, double ransacReprojThreshold, size_t maxIters, double confidence, size_t refineIters) {
-//    return new cv::Mat(cv::estimateAffine2D(*from, *to, *inliers, method, ransacReprojThreshold, maxIters, confidence, refineIters));
-//}
+Mat EstimateAffine2DWithParams(Point2fVector from, Point2fVector to, Mat inliers, int method, double ransacReprojThreshold, size_t maxIters, double confidence, size_t refineIters) {
+    return new cv::Mat(cv::estimateAffine2D(*from, *to, *inliers, method, ransacReprojThreshold, maxIters, confidence, refineIters));
+}
